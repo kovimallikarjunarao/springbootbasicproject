ef49f730e422108267cfc9d713e88cb0580cac09
Narturebelle <kumbamvijayvenkatreddy956@gmail.com>
2025-12-15 21:50:12 -0500

    Added new method

--- src/main/java/com/example/Interview/StudentService.java
+++ src/main/java/com/example/Interview/StudentService.java
@@ -13,6 +13,10 @@ public class StudentService {
     public Student saveStudent(Student student){
         return repository.save(student);
     }
+
+    public void getVoidMethod(){
+        System.out.println("I am form get method!");
+    }
     public Iterable<Student> getAllStudents(){
         return repository.findAll();
     }
