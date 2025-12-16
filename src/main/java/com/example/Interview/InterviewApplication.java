4f6af4067baff7bca4996e0ac3cbbbd099adfc30
kovimallikarjunarao <mallikarjunkovi050@gmail.com>
2025-12-15 18:30:50 -0600

    removed the second change

--- src/main/java/com/example/Interview/InterviewApplication.java
+++ src/main/java/com/example/Interview/InterviewApplication.java
@@ -8,7 +8,7 @@ public class InterviewApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InterviewApplication.class, args);
-        System.out.println("Hello, World!");
+        
 	}
 
 }
